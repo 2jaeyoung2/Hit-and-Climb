84696d08b377e00b554351af24fa082bbbf1b6b7
2jaeyoung2 <i0579923@gmail.com>
2025-07-20 18:34:04 +0900

    [Temp]move in progress

--- Assets/01. Scripts/PlayerMovement.cs
+++ Assets/01. Scripts/PlayerMovement.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 moveDirection;
+
+    private void FixedUpdate()
     {
-        
+        // moveDirection(방향)이 있다면
+        if (moveDirection != Vector2.zero)
+        {
+            //if()
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputAction.CallbackContext ctx)
     {
-        
+        if (ctx.phase == InputActionPhase.Performed)
+        {
+            moveDirection = ctx.ReadValue<Vector2>().normalized;
+        }
+        else if (ctx.phase == InputActionPhase.Canceled)
+        {
+            moveDirection = Vector2.zero;
+        }
     }
 }
